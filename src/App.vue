8acdfcc70f6ac2dae0c18155fece9c7b356e06ab
Infant Frontender <newbornfrontender@gmail.com>
2018-08-10 14:43:20 +0300

    Добавил свойства для теста autoprefixer'а

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,10 @@
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  animation-timing-function: linear;
+  transition-timing-function: ease;
 }
 #nav {
   padding: 30px;
